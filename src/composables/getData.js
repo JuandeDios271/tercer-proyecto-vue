60af4f28d382bf1d2b3f6cab5665b8688e558197
Juan Garcia <garciaperezjuandedios2001@gmail.com>
2023-11-13 12:33:23 -0600

    Actualizacines seccion 6

--- src/composables/getData.js
+++ src/composables/getData.js
@@ -3,21 +3,27 @@ import { ref } from 'vue';
 
 export const useGetData = () => {
 
-    const data = ref(null)
+    const data = ref(null);
+    const error = ref(null);
+    const loading = ref(true)
 
 
     const getData = async (url) => {
+        loading.value = true;
         try {
             const res = await axios.get(url);
             data.value = res.data;
-        } catch (error) {
-            console.log(error)
-            
+        } catch (e) {
+            error.value = "Error de servidor"
+        } finally {
+            loading.value = false;
         }
     };
 
     return {
         getData,
-        data
+        data,
+        loading,
+        error,
     }
 }
